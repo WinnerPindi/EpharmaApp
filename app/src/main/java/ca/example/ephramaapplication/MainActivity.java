c0fe8417bd9f1e3ef70ab25dccef12e308ea39e0
pindi <pindi@uqtr.ca>
2022-07-15 13:07:59 -0400

    Ajout des recyclerView (Vertical et horizontal)

--- app/src/main/java/ca/example/ephramaapplication/MainActivity.java
+++ app/src/main/java/ca/example/ephramaapplication/MainActivity.java
@@ -1,19 +1,25 @@
 package ca.example.ephramaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.os.Handler;
+
+import ca.example.ephramaapplication.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-
-
+        //Injecter le fragment dans notre boite (fragment_container)
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new HomeFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
 
     }
 }
